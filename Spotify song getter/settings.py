5e2102e2d6233292963a4d8ee49ccf842197f916
Benedikt00 <88801130+Benedikt00@users.noreply.github.com>
2024-01-10 20:38:37 +0100

    Update settings.py

--- Spotify song getter/settings.py
+++ Spotify song getter/settings.py
@@ -1,13 +1,1 @@
 
-
-root_folder = r"C:\Users\bsimb\Music\Musik"
-wix_alles_eine = r"C:\Users\bsimb\Music\Musik\Dies und jenes"
-
-
-
-spotify_stuff = {"id": client_id, "secret": client_secret}
-
-output_csv = "track_info.csv"
-
-artist_file = "Artists.txt"
-
